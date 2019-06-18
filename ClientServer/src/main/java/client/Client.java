668ae981203bd95793784bf38bf9a16e3366bdf5
Vermodje <mr.ryabchikov@list.ru>
2019-06-18 23:28:07 +0300

    18.06.2019

--- ClientServer/src/main/java/client/Client.java
+++ ClientServer/src/main/java/client/Client.java
@@ -4,19 +4,39 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args) {
-        try {
-            Socket clientSocket = new Socket("127.0.0.1", 8080);
-            BufferedReader buf = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(clientSocket.getOutputStream());
-            outputStreamWriter.write("Hello World\n");
-            outputStreamWriter.flush();
-            System.out.println(buf.readLine());
-            outputStreamWriter.close();
-            buf.close();
-            clientSocket.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    private static Socket clientSocket;
+    private static BufferedReader reader; // нам нужен ридер читающий с консоли, иначе как
+    // мы узнаем что хочет сказать клиент?
+    private static BufferedReader in;
+    private static BufferedWriter out;
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+                clientSocket = new Socket("localhost", 4004);
+
+                reader = new BufferedReader(new InputStreamReader(System.in));
+
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                while (!clientSocket.isClosed()){
+                    System.out.println("Вы что-то хотели сказать? Введите это здесь:");
+
+                    String word = reader.readLine();
+                    out.write(word + "\n");
+                    if(word.equalsIgnoreCase("quit")){
+                        out.flush();
+                        System.out.println(in.readLine());
+                        Thread.sleep(3000);
+                        break;
+                    }
+                    out.flush();
+                    String serverWord = in.readLine();
+                    System.out.println(serverWord);
+                }
+
+                clientSocket.close();
+                in.close();
+                out.close();
+
+
     }
 }
--- ClientServer/src/main/java/server/Server.java
+++ ClientServer/src/main/java/server/Server.java
@@ -1,41 +1,40 @@
 package server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
+    private static Socket clientSocket; //сокет для общения
+    private static ServerSocket server; // серверсокет
+    private static BufferedReader in; // поток чтения из сокета
+    private static BufferedWriter out; // поток записи в сокет
+    public static void main(String[] args) throws IOException, InterruptedException {
+                server = new ServerSocket(4004);
+                System.out.println("Сервер запущен!");
 
-    public static void main(String[] args) {
-        try {
-            int count = 0;
-            ServerSocket serverSocket = new ServerSocket(8080);
-            System.out.println("Server started");
-            while (true){
-                Socket clientSocket = serverSocket.accept();//Создаем канал для общения,
-                // сервер ожидает подключение, когда кто-то перейдет по адресу http://127.0.0.1:8080
-                BufferedReader buf = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                //Создаем выходной буфер
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(clientSocket.getOutputStream());
-                String req = buf.readLine();
-                String resp  = "You asked me" + " " + req.length();
+                clientSocket = server.accept();
+        System.out.println("A new client is connected : " + clientSocket);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+                while (!clientSocket.isClosed()) {
+                    String word = in.readLine();
+                    if(word.equalsIgnoreCase("quit")){
+                        System.out.println("Client initialize connections suicide ...");
+                        out.write("Вы покинули сервер");
+                        out.flush();
+                        Thread.sleep(3000);
+                        break;
+                    }
+                    out.write("Привет, это Сервер! Подтверждаю, вы написали : " + word + "\n");
+                    out.flush();
+                }
 
-                //Записываем в буфер
-                outputStreamWriter.write(resp);
-                //Очищаем буфер
-                outputStreamWriter.flush();
-                outputStreamWriter.close();
-                buf.close();
-                //Закрываем буфер
-                clientSocket.close();
-            }
+                    clientSocket.close();
+                    in.close();
+                    out.close();
+                    System.out.println("Сервер закрыт!");
+                    server.close();
 
-            //serverSocket.close();//После этого сервер будет закрыт
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
